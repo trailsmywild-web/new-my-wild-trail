5758841d0d6c8ed4d2f5c3b3997a873661e1d9e4
trailsmywild <trailsmywild@gmail.com>
2026-02-12 11:56:26 +0530

    Ready for Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://wild-trails-my.web.app'
+    'https://wild-trails-my.web.app',
+    'https://wild-trails-my.firebaseapp.com'
   ],
   credentials: true
 }));
